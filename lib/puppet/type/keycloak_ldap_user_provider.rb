6ae92a792658f23572d227b804c2522acec5f5a3
Nick Bertrand <bert0214@umn.edu>
2025-09-18 13:24:09 -0500

    Add support for EVICT_* and MAX_LIFESPAN cache policies

--- lib/puppet/type/keycloak_ldap_user_provider.rb
+++ lib/puppet/type/keycloak_ldap_user_provider.rb
@@ -103,6 +103,22 @@ Manage Keycloak LDAP user providers
     defaultto 'DEFAULT'
   end
 
+  newproperty(:eviction_day) do
+    desc 'evictionDay'
+  end
+
+  newproperty(:eviction_hour) do
+    desc 'evictionHour'
+  end
+
+  newproperty(:eviction_minute) do
+    desc 'evictionMinute'
+  end
+
+  newproperty(:max_lifespan) do
+    desc 'maxLifespan'
+  end
+
   newproperty(:batch_size_for_sync) do
     desc 'batchSizeForSync'
     defaultto '1000'
@@ -260,6 +276,30 @@ Manage Keycloak LDAP user providers
     end
     if self[:bind_dn] && self[:auth_type] == 'none'
       raise Puppet::Error, 'bind_dn is not valid for auth_type none'
+    end
+    if ['EVICT_DAILY', 'EVICT_WEEKLY'].include?(self[:cache_policy].to_s) && self[:eviction_hour].nil?
+      raise Puppet::Error, 'cache_policy EVICT_DAILY and EVICT_WEEKLY require eviction_hour'
+    end
+    if ['EVICT_DAILY', 'EVICT_WEEKLY'].include?(self[:cache_policy].to_s) && self[:eviction_minute].nil?
+      raise Puppet::Error, 'cache_policy EVICT_DAILY and EVICT_WEEKLY require eviction_minute'
+    end
+    if !['EVICT_DAILY', 'EVICT_WEEKLY'].include?(self[:cache_policy].to_s) && !self[:eviction_hour].nil?
+      raise Puppet::Error, 'eviction_hour is only valid for cache_policy EVICT_DAILY and EVICT_WEEKLY'
+    end
+    if !['EVICT_DAILY', 'EVICT_WEEKLY'].include?(self[:cache_policy].to_s) && !self[:eviction_minute].nil?
+      raise Puppet::Error, 'eviction_minute is only valid for cache_policy EVICT_DAILY and EVICT_WEEKLY'
+    end
+    if self[:cache_policy].to_s == 'EVICT_WEEKLY' && self[:eviction_day].nil?
+      raise Puppet::Error, 'cache_policy EVICT_WEEKLY requires eviction_hour'
+    end
+    if self[:cache_policy].to_s != 'EVICT_WEEKLY' && !self[:eviction_day].nil?
+      raise Puppet::Error, 'eviction_day is only valid with cache_policy EVICT_WEEKLY'
+    end
+    if self[:cache_policy].to_s == 'MAX_LIFESPAN' && self[:max_lifespan].nil?
+      raise Puppet::Error, 'cache_policy MAX_LIFESPAN requires max_lifespan'
+    end
+    if self[:cache_policy].to_s != 'MAX_LIFESPAN' && !self[:max_lifespan].nil?
+      raise Puppet::Error, 'max_lifespan is only valid with cache_policy MAX_LIFESPAN'
     end
   end
 end
